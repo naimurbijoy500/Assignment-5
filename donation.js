7a9de6f75f6eed268bf9eec3b3838ea2dc66e84e
naimurbijoy500 <naimurasif99@gmail.com>
2024-09-25 12:58:36 +0600

    rahman

--- donation.js
+++ donation.js
@@ -1,32 +1,24 @@
 
+
 console.log(document.getElementsByClassName('donation-btn'))
 const btns=document.getElementsByClassName('donation-btn')
 for(const btn of btns){
     btn.addEventListener("click",(e)=>{
       
       console.log(e.target.parentNode.children[3].value)
-   
     if(!isNaN(e.target.parentNode.children[3].value) && parseFloat(e.target.parentNode.children[3].value)>0){
       e.preventDefault()
-
-openModalButton.addEventListener('click', () => {
-  modal.showModal();
-});
-
-closeModalButton.addEventListener('click', () => {
-  modal.close();
-});
-
+    
 
 
 
       e.target.parentNode.children[0].children[1].innerText=parseFloat(e.target.parentNode.children[3].value)+parseFloat(e.target.parentNode.children[0].children[1].innerText)+" BDT"
-      document.getElementById('available-amount').innerText=parseFloat(document.getElementById('available-amount').innerText)-parseFloat(e.target.parentNode.children[3].value)
+      document.getElementById('available-amount').innerText=parseFloat(document.getElementById('available-amount').innerText)-parseFloat(e.target.parentNode.children[3].value)+"BDT"
       const div=document.createElement('div')
       const htwo=document.createElement('h1')
       const paragraph=document.createElement('p')
       htwo.innerText=e.target.parentNode.children[3].value+' tk Donated for '+e.target.parentNode.children[1].children[0].innerText;
-     
+    
       paragraph.innerHTML="Date: "+new Date().toString()
       div.appendChild(htwo)
       div.appendChild(paragraph)
@@ -36,6 +28,8 @@ closeModalButton.addEventListener('click', () => {
       divContainer.classList.add("p-7")
       divContainer.appendChild(div)
       document.getElementById('history-part').appendChild(divContainer)
+      document.getElementById('my_modal_1').showModal()
+
     } else{
       alert('Invalid amount') 
     }
